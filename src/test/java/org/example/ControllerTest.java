c8558e2fd3699fa3b055726cd1329f7e6b350394
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-11-04 17:21:17 -0300

    test: ControllerTest.java con ejemplo de consigna

---
+++ src/test/java/org/example/ControllerTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void testConsigna(){
+        String input = """
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+""";
+
+        String output = "1 3 N\n" +
+                "5 1 E";
+        Controller controller = new Controller();
+        String[] args = input.split("\n");
+        String result = controller.processInput(args);
+        assertEquals(output, result);
+
+    }
+
+}
